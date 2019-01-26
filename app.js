020c063186b7ac8a22bbec3fc9f1d3d8a0418447
Ryan Peterson <petersonryan97@gmail.com>
2019-01-26 13:53:57 -0800

    Added api keys

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var Instafeed = require('instafeed.js');
+var api_keys  = require('../api_keys.json');
+
 var app = express();
 
 // view engine setup
